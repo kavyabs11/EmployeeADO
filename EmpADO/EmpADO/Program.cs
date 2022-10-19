07914502f1aa57c6b0c6a609a66b994660bb25ee
kavyabs11 <kavyasomshekar11@gmail.com>
2022-10-19 20:14:05 +0530

    emppayroll

---
+++ EmpADO/EmpADO/Program.cs
@@ -0,0 +1,31 @@
+﻿using EmpADO;
+
+namespace EmployeePayRollADO
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wel-Come to Employee payroll");
+
+            EmployeeModel model = new EmployeeModel();
+            EmployeeRepo repo = new EmployeeRepo();
+
+            model.EmployeeName = "Bruce";
+            model.PhoneNumber = 1234567890;
+            model.Address = "Banglore";
+            model.Department = "HR";
+            model.Gender = 'M';
+            model.BasicPay = 22000;
+            model.Deduction = 1500;
+            model.TaxablePay = 200;
+            model.Tax = 300;
+            model.NetPay = 2500;
+            model.City = "Benglore";
+            model.Country = "India";
+
+            //repo.AddEmployee(model);
+            repo.GetAllEmployee();
+        }
+    }
+}
